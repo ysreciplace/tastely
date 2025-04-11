f8e0284caf3776337a0b5205eae621272b161418
Chan01119 <cykimjejus112@gmail.com>
2025-04-11 17:41:17 +0900

    chore : 팀 프로젝트 초기 기반

---
+++ src/main/java/org/ysreciplace/tastely/TastelyApplication.java
@@ -0,0 +1,13 @@
+package org.ysreciplace.tastely;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TastelyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TastelyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/ysreciplace/tastely/TastelyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ysreciplace.tastely;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TastelyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
