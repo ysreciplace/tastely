1a77c6d925b7428fd96ae15d7c75bdeedce3317c
KMS <komi254@naver.com>
2025-04-14 23:48:47 +0900

    feat:thumbnailFile 등록

--- src/main/java/org/ysreciplace/tastely/controller/RecipeController.java
+++ src/main/java/org/ysreciplace/tastely/controller/RecipeController.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.ysreciplace.tastely.entity.Ingredient;
 import org.ysreciplace.tastely.entity.Recipe;
 import org.ysreciplace.tastely.entity.Step;
@@ -12,7 +13,12 @@ import org.ysreciplace.tastely.repository.RecipeRepository;
 import org.ysreciplace.tastely.repository.StepRepository;
 import org.ysreciplace.tastely.request.NewRecipe;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.UUID;
 
 @Controller
 @RequestMapping("/recipe")
@@ -29,11 +35,26 @@ public class RecipeController {
 
     @PostMapping("/history")
     public String historyPostHandel(@ModelAttribute NewRecipe newRecipe,
-                                    Model model) {
+                                    @RequestParam("thumbnailFile") MultipartFile thumbnailFile,
+                                    Model model) throws IOException {
+
+        Recipe recipe = newRecipe.getRecipe();
+        String uploadDir = System.getProperty("user.dir") + "/src/main/resources/static/uploads/thumbnails/";
+        if (!thumbnailFile.isEmpty()) {
+            String originalFilename = thumbnailFile.getOriginalFilename();
+            String savedFilename = UUID.randomUUID() + "_" + originalFilename;
+            Path savedPath = Paths.get(uploadDir,savedFilename);
+
+            Files.createDirectories(savedPath.getParent());
+            thumbnailFile.transferTo(savedPath.toFile());
+            //썸네일 경로를 DB에 저장
+            recipe.setThumbnail("/" + uploadDir + savedFilename);
+        }
 
         System.out.println("userId = " + newRecipe.getRecipe().getUserId());
         System.out.println("=========? "+  newRecipe.getRecipe().getId());
-        recipeRepository.save( newRecipe.getRecipe());
+        recipeRepository.save(recipe);
+        //recipeRepository.save(newRecipe.getRecipe());
         System.out.println("=======================? "+  newRecipe.getRecipe().getId());
 
         for (Ingredient one: newRecipe.getIngredients() ) {
