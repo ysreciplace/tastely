615729dd6865a278e5bbce767fd90f5a78089c22
yeon <skykys8@gmail.com>
2025-04-15 12:02:43 +0900

    feat : 홈화면 인기레시피 이미지와 제목 구현함

--- src/main/java/org/ysreciplace/tastely/controller/RankingController.java
+++ src/main/java/org/ysreciplace/tastely/controller/RankingController.java
@@ -1,9 +1,9 @@
 package org.ysreciplace.tastely.controller;  // 패키지 경로 수정
 
-import org.ysreciplace.tastely.model.Recipe;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.ysreciplace.tastely.entity.Recipe;
 
 import java.util.Arrays;
 import java.util.List;
@@ -16,11 +16,12 @@ public class RankingController {
     public String home(Model model) {
         // 음식 목록 데이터 (임시 데이터)
         List<Recipe> recipes = Arrays.asList(
-                new Recipe("스키야키", "맛있는 스키야키 레시피", "/images/sukiyaki.jpg"),
-                new Recipe("감바스", "무드있는 감바스 레시피", "/images/gambas.jpg"),
-                new Recipe("타코", "멕시코 현지에 온 듯한 타코 레시피", "/images/taco.jpg"),
-                new Recipe("감바스", "무드있는 감바스 레시피", "/images/gambas.jpg"),
-                new Recipe("감바스", "무드있는 감바스 레시피", "/images/gambas.jpg")
+                Recipe.builder().title("스키야키").description( "맛있는 스키야키 레시피").thumbnail("/images/sukiyaki.jpg").build(),
+                Recipe.builder().title("감바스").description( "무드있는 감바스 레시피").thumbnail("/images/gambas.jpg").build(),
+                Recipe.builder().title("타코").description( "멕시코 현지에 온 듯한 타코 레시피").thumbnail("/images/taco.jpg").build(),
+                Recipe.builder().title("피자").description( "맛있는 스키야키 레시피").thumbnail("/images/sukiyaki.jpg").build(),
+                Recipe.builder().title("스키야키").description( "맛있는 스키야키 레시피").thumbnail("/images/sukiyaki.jpg").build()
+
         );
 
         // 모델에 레시피 데이터 추가
