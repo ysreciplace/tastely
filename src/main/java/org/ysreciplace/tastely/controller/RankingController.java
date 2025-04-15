cc7819071972e61292c9b96a8c5fca6c7cad4ad1
yeon <skykys8@gmail.com>
2025-04-15 12:14:29 +0900

    feat : 홈화면 인기레시피 이미지와 제목 구현함 (수정본)

--- src/main/java/org/ysreciplace/tastely/controller/RankingController.java
+++ src/main/java/org/ysreciplace/tastely/controller/RankingController.java
@@ -19,8 +19,8 @@ public class RankingController {
                 Recipe.builder().title("스키야키").description( "맛있는 스키야키 레시피").thumbnail("/images/sukiyaki.jpg").build(),
                 Recipe.builder().title("감바스").description( "무드있는 감바스 레시피").thumbnail("/images/gambas.jpg").build(),
                 Recipe.builder().title("타코").description( "멕시코 현지에 온 듯한 타코 레시피").thumbnail("/images/taco.jpg").build(),
-                Recipe.builder().title("피자").description( "맛있는 스키야키 레시피").thumbnail("/images/sukiyaki.jpg").build(),
-                Recipe.builder().title("스키야키").description( "맛있는 스키야키 레시피").thumbnail("/images/sukiyaki.jpg").build()
+                Recipe.builder().title("피자").description( "치즈가 쭉쭉 늘어나는 피자 레시피").thumbnail("/images/pizza.jpg").build(),
+                Recipe.builder().title("팟타이").description( "태국의 정통 팟타이 레시피").thumbnail("/images/padthai.jpg").build()
 
         );
 
