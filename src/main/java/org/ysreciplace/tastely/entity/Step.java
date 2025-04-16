02e0da3a1348c48bc3ee82d8e49e052d9aa6e7ff
yeon <skykys8@gmail.com>
2025-04-16 14:56:30 +0900

    feat : 홈화면 페이지 구축함(다 꾸밈), 썸네일 누르면 상세페이지로 넘어가고 레시피 띄우는 웹페이지를 구현함

---
+++ src/main/java/org/ysreciplace/tastely/entity/Step.java
@@ -0,0 +1,17 @@
+package org.ysreciplace.tastely.entity;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Step {
+
+    private Long id;
+    private Long recipeId;
+    private int stepNumber;
+    private String description;
+    private String image;
+}
--- src/main/java/org/ysreciplace/tastely/repository/RecipeRepository.java
+++ src/main/java/org/ysreciplace/tastely/repository/RecipeRepository.java
@@ -1,7 +1,10 @@
 package org.ysreciplace.tastely.repository;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.ysreciplace.tastely.entity.Ingredient;
 import org.ysreciplace.tastely.entity.Recipe;
+import org.ysreciplace.tastely.entity.Step;
 
 @Mapper
 public interface RecipeRepository {
